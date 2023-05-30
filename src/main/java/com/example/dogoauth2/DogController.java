e08c8918e0397136554430e670fa394b5fc44168
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-05-30 16:26:33 +0200

    Simplify retrieval of access token
    MANY thanks to Marcell!

--- src/main/java/com/example/dogoauth2/DogController.java
+++ src/main/java/com/example/dogoauth2/DogController.java
@@ -1,20 +1,34 @@
 package com.example.dogoauth2;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 @RestController
 @RequestMapping("/api/dogs")
+@RequiredArgsConstructor
 public class DogController {
+
+    private final OAuth2AuthorizedClientService clientService;
 
     @GetMapping
     public String getDog() {
+        return clientService.loadAuthorizedClient("github", getPrincipalName())
+                .getAccessToken()
+                .getTokenValue();
+    }
+
+    private static String getPrincipalName() {
         var principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        if (principal instanceof CustomOauth2User) {
-            return "Access Token: " + ((CustomOauth2User) principal).getAccessToken();
+        if (principal instanceof DefaultOAuth2User user) {
+            return Objects.toString(user.getAttribute("id"));
         }
-        return "Woof";
+        throw new IllegalStateException("No principal found");
     }
 }
